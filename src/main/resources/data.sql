41b9beb3ce29f14ffb4a88175da11b960a1d3928
Gayatri <rajangayathri65@gmail.com>
2020-02-29 00:42:20 +0530

    JPA Base RestService Development completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101,'rajan@gmail.com','Rajan','Pandiyarajan','admin','ssn101','RRajan');
+insert into user values(102,'surya@gmail.com','Surya','Rajan','admin','ssn102','SRajan');
+insert into user values(103,'gayatri@gmail.com','Gayatri','Rajan','admin','ssn103','GRajan');
