d482666880d9333795a95e02ade7bdfd7013f1a6
Gayatri <rajangayathri65@gmail.com>
2020-03-02 18:31:20 +0530

    Json Ignore Commit

--- src/main/java/com/stacksimplify/restservices/entities/User.java
+++ src/main/java/com/stacksimplify/restservices/entities/User.java
@@ -13,8 +13,12 @@ import javax.validation.constraints.Size;
 
 import org.springframework.hateoas.RepresentationModel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "user")
+@JsonIgnoreProperties({"firstname","lastname"})
 public class User extends RepresentationModel {
 
 	@Id
@@ -32,7 +36,8 @@ public class User extends RepresentationModel {
 	private String email;
 	@Column(name = "ROLE", length = 50, nullable = false)
 	private String role;
-	@Column(name = "SSN", length = 50, nullable = false, unique = true)
+	@Column(name = "SSN", length = 50, nullable = true, unique = true)
+	@JsonIgnore
 	private String ssn;
 
 	@OneToMany(mappedBy = "user")
